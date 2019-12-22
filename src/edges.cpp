898c4adc09c3457523273cf698f6465eb6582b87
mocobt <mocobt@gmail.com>
2019-12-22 21:38:56 +0900

    Implemented

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,35 @@
 #include "edges.h"
+#include <algorithm>
+#include <set>
 
-Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
-{
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+Eigen::MatrixXi edges(const Eigen::MatrixXi &F) {
+  Eigen::MatrixXi E(F.rows() * 3 / 2, 2);
+
+  // Add all edges to the set
+  std::set<std::pair<int, int>> edges;
+  int min_v, max_v;
+  for (int i = 0; i < F.rows(); i++) {
+    min_v = std::min(F(i, 0), F(i, 1));
+    max_v = std::max(F(i, 0), F(i, 1));
+    edges.insert(std::make_pair(min_v, max_v));
+
+    min_v = std::min(F(i, 1), F(i, 2));
+    max_v = std::max(F(i, 2), F(i, 1));
+    edges.insert(std::make_pair(min_v, max_v));
+
+    min_v = std::min(F(i, 2), F(i, 0));
+    max_v = std::max(F(i, 2), F(i, 0));
+    edges.insert(std::make_pair(min_v, max_v));
+  }
+
+  // Assign each edge to E
+  int edge_index = 0;
+  for (std::set<std::pair<int, int>>::iterator e = edges.begin();
+       e != edges.end(); e++) {
+    std::pair<int, int> edge = *e;
+    E(edge_index, 0) = edge.first;
+    E(edge_index++, 1) = edge.second;
+  }
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,13 @@
 #include "euler_characteristic.h"
+#include <iostream>
+#include "edges.h"
 
-int euler_characteristic(const Eigen::MatrixXi &F)
-{
-  int Chi = 0;
-  // ADD YOUR CODE HERE
+int euler_characteristic(const Eigen::MatrixXi &F) {
+  Eigen::MatrixXi E = edges(F);
+  // |V| = E.maxCoeff() + 1
+  // |E| = E.rows()
+  // |F| = F.rows()
+  // Chi = |V| - |E| + |F|
+  int Chi = E.maxCoeff() + 1 - E.rows() + F.rows();
   return Chi;
 }
